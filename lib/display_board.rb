146f027b56b428325903b86f9c924a80c84fe6f8
Jasmine Carter <jasmine.carter87@gmail.com>
2019-04-30 00:26:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,46 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-def display_board(board)
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-end
-
-board = [" "," "," "," ","X"," "," "," "," "]
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
-  puts " O |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board(board)
-  puts " O |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   | X "
-end
-
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-def display_board(board)
-  puts " X | X | X "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+rows = "#{board[0]}|#{board[1]}|#{board[2]}\n-----------\n#{board[3]}|#{board[4]}|#{board[5]}\n-----------\n#{board[6]}|#{board[7]}|#{board[8]}"
+def
+display_board(board)
+puts rows.split("\n")
 end
