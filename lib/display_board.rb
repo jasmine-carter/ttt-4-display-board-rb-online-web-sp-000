d1c4b01db1d3ad697b2853ecad9b30337b94daea
Jasmine Carter <jasmine.carter87@gmail.com>
2019-04-30 00:37:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+#empty board state
 board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
-rows = "#{board[0]}|#{board[1]}|#{board[2]}\n-----------\n#{board[3]}|#{board[4]}|#{board[5]}\n-----------\n#{board[6]}|#{board[7]}|#{board[8]}"
-def
-display_board(board)
-puts rows.split("\n")
+def display_board(board)
+puts "#{board[0]}|#{board[1]}|#{board[2]}\n-----------\n#{board[3]}|#{board[4]}|#{board[5]}\n-----------\n#{board[6]}|#{board[7]}|#{board[8]}"
 end
+
+display_board(board)
